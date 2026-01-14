a11c2b81bda300e0714668b3e0a448bd798f5564
Ruwy123 <123378428+Ruwy123@users.noreply.github.com>
2026-01-14 01:04:30 +0000

    push

--- shoply/app/layout.tsx
+++ shoply/app/layout.tsx
@@ -1,8 +1,8 @@
+import { ThemeProvider } from "next-themes";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_NAME, APP_DESCRIPTION, APP_URL } from "@/lib/constants";
-import { ThemeProvider } from "next-themes";
 
 const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
 
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${poppins.className} antialiased`}>
+      <body className={`${poppins.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
--- shoply/components/shared/header/index.tsx
+++ shoply/components/shared/header/index.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
+    <header className="w-full border">
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
@@ -23,13 +24,14 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingBag />
               Cart
             </Link>
           </Button>
-          <Button asChild>
+          <Button asChild className="bg-black text-white">
             <Link href="/sign-in">
               <UserIcon />
               Sign In
